1505eadbbe53bcc5b17971a3b835be32a7b17bb8
dajtoryu <dajtoryu@gmail.com>
2025-10-30 22:29:23 +0200

    Added PWA service worker and manifest

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,15 @@ import App from "./App.jsx";
 import { registerSW } from "virtual:pwa-register";
 
 registerSW();
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then(() => console.log("SW registered"))
+      .catch((err) => console.log("SW registration failed:", err));
+  });
+}
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
